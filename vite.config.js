d8595dd4b91ab9bb527f217ebafdfd837653f940
unknown <foo424@naver.com>
2025-08-14 15:49:07 +0900

    fix: update config and styles

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/profile/",
-  plugins: [react({ jsxRuntime: "automatic" })],
+  plugins: [react()],
+  base: "/ProfilePage/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
 });
